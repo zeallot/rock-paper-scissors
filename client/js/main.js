4e051a0ba22abeefa3e5e56ac63e8c056ccffb88
vkaboskin <kookladesu@gmail.com>
2020-08-19 19:15:47 +0700

    add readme, loader (in progress)

--- client/js/main.js
+++ client/js/main.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const BASE_URL = 'http://localhost:3000';
+    let isLoaded = false;
 
     const bot1TotalWins = document.querySelector('#bot-1-total-wins');
     const bot2TotalWins = document.querySelector('#bot-2-total-wins');
+    const totalDraws = document.querySelector('#total-draws');
+    const totalGame = document.querySelector('#total-games');
+    const loader = document.querySelector('#loader');
 
     const fetchGameInfo = async () => {
         try {
             const res = await axios.get(`${BASE_URL}/game`);
             const game = res.data;
             const { gameInfo: { lastGameResult: { bot1Sign, bot2Sign }}} = game;
-            const { gameInfo: { wins_bot_1, wins_bot_2 }} = game;
+            const { gameInfo: { wins_bot_1, wins_bot_2, count, draw }} = game;
             bot1TotalWins.innerHTML = wins_bot_1;
             bot2TotalWins.innerHTML = wins_bot_2;
+            totalDraws.innerHTML = draw;
+            totalGame.innerHTML = count;
+            if (!isLoaded) {
+                loader.classList.remove('show');
+                isLoaded = true;
+            }
         } catch (e) {
             console.error(e);
         }
